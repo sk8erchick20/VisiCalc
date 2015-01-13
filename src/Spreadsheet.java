5ffd5ddb89af08406adbe989a4ddf1faeee80cde
Karishma <tikkysood@yahoo.com>
2015-01-12 18:46:28 -0700

    Update Spreadsheet.java

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,4 +1,4 @@
-public class Spreadsheet 
+public class Spreadsheet
 {
   private Cell[][] cells;
   private static int rows=11;
@@ -7,35 +7,32 @@ public class Spreadsheet
   private static int asciiOffset=64;
   private String border;
   private String borderRow;
-  // private methods
-  private static int getCol(String location) 
+// private methods
+  private static int getCol(String location)
   {
-    return (int)location.charAt(0)-asciiOffset;  
+    return (int)location.charAt(0)-asciiOffset;
   }
-  
-  private static int getRow(String location) 
+  private static int getRow(String location)
   {
     return Integer.parseInt(location.substring(1));
   }
-  
   private static String padOrTruncateDisplayString(Cell cell) {
-    // truncate string to length - 1 and display >
+// truncate string to length - 1 and display >
     String s = cell.getDisplayString();
     if (s.length() > colWidth) {
       s = s.substring(0, colWidth - 1) + ">";
     }
-    // if length is odd pad end to even length
+// if length is odd pad end to even length
     if (s.length() % 2 == 1) {
       s += " ";
     }
-    // justify pad to center in cell
+// justify pad to center in cell
     while (s.length() < colWidth) {
       s = " " + s + " ";
     }
     return s;
   }
-  
-  // constructor
+// constructor
   public Spreadsheet() {
     cells= new Cell[rows][cols];
     cells[0][0]=new CellString("");
@@ -59,26 +56,23 @@ public class Spreadsheet
       }
     }
   }
-  
-  // mutators
+// mutators
   public void setCell(String location, Cell cell) {
     cells[getRow(location)][getCol(location)]=cell;
   }
-  
-  // accessors
+// accessors
   public Cell getCell(String location) {
     return cells[getRow(location)][getCol(location)];
   }
-  
   public void print() {
-    for(int row=0; row<rows; row++)
+    for (int row = 0; row< rows; row++)
     {
-      for(int col=0; col<cols; col++)
+      for(int col = 0; col<cols; col++)
       {
         System.out.print(padOrTruncateDisplayString(cells[row][col]));
         System.out.print("|");
       }
-      System.out.println(borderRow);
-    }   
+      System.out.println("\n"+borderRow);
+    }
   }
 }
